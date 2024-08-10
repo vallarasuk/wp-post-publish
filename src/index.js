70e550f703c8de430c4f2badd6b1f91ff69a70de
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2024-08-10 22:19:09 +0530

    faeture/post component added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import PublishPost from "./Components/PublishPost";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PublishPost />
   </React.StrictMode>
 );
 
